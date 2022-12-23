7c59f20aa4891cbe48a6021fb3fd2587fad05dda
MultimedieGunvor <90311518+MultimedieGunvor@users.noreply.github.com>
2022-12-23 11:04:25 +0100

    Initial commit

---
+++ src/components/DragDrop.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Picture from "./Picture";
+import { useDrop } from "react-dnd";
+import "../App.css";
+import jakke from "../images/Jakke.png"
+import jeans from "../images/Jeans.png"
+import kjole from "../images/Kjole.png"
+import top from "../images/Top.png"
+import minijakke from "../images/Minijakke.png"
+
+const PictureList = [
+  {
+    id: 1,
+    url: jeans,
+  },
+  {
+    id: 2,
+    url: jakke,
+  },
+  {
+    id: 3,
+    url: kjole,
+  },
+  {
+    id: 4,
+    url: top,
+  },
+  {
+    id: 5,
+    url: minijakke,
+  },
+];
+
+function DragDrop() {
+  const [board, setBoard] = useState([]);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "image",
+    drop: (item) => addImageToBoard(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addImageToBoard = (id) => {
+    const pictureList = PictureList.filter((picture) => id === picture.id);
+    setBoard((board) => [...board, pictureList[0]]);
+  };
+  return (
+    <>
+      <div className="Board" ref={drop}>
+        {board.map((picture) => {
+          return <Picture url={picture.url} id={picture.id} />;
+        })}
+      </div>
+      <div className="Pictures">
+        {PictureList.map((picture) => {
+          return <Picture url={picture.url} id={picture.id} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default DragDrop;
